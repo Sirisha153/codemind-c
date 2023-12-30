5f393735e4094cc2ebb5d91acf108a6bfaa57ddf
Sirisha153 <147491033+Sirisha153@users.noreply.github.com>
2023-12-30 22:20:33 +0530

    #include<stdio.h> int main() {     int n,i,j,k;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(k=1;k<=i-1;k++)         {             printf(" ");         }         for(j=1;j<=n-i+1;j++)         {             printf("%c ",64+i);         }         printf(" ");     } }

--- Pattern_6.5.c
+++ Pattern_6.5.c
@@ -1,13 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j;
+    int n,i,j,k;
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
+        for(k=1;k<=i-1;k++)
+        {
+            printf(" ");
+        }
         for(j=1;j<=n-i+1;j++)
         {
-            printf("%d ",j);
+            printf("%c ",64+i);
         }
         printf("
 ");
